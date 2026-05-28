aec5b221c29770094dde4658c61535af27db1679
nananina <nananina0415@gmail.com>
2026-05-28 18:40:43 +0900

    feat: 출석 체크 페이지 분리 및 라우팅 변경

--- src/pages/member/attendance/index.tsx
+++ src/pages/member/attendance/index.tsx
@@ -1,22 +1,10 @@
-import { useParams, useSearchParams } from "react-router-dom";
 import SimpleLogoLayout from "../../../layouts/SimpleLogoLayout";
 import AttendanceForm from "../../../features/attendance/AttendanceForm";
-import AttendanceResult from "../../../features/attendance/AttendanceResult";
 
 export default function AttendancePage() {
-  const { scheduleId } = useParams<{ scheduleId: string }>();
-  const [searchParams] = useSearchParams();
-  const code = searchParams.get("code");
-
-  if (!scheduleId) return null;
-
   return (
     <SimpleLogoLayout>
-      {code !== null ? (
-        <AttendanceResult scheduleId={scheduleId} code={code} />
-      ) : (
-        <AttendanceForm scheduleId={scheduleId} />
-      )}
+      <AttendanceForm />
     </SimpleLogoLayout>
   );
 }
