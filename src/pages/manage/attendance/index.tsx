db367db6d4dbc844f09c90c651b030518b6a7fa4
nananina <nananina0415@gmail.com>
2026-06-20 18:33:16 +0900

    revert: 출석 관련 불필요한 변경 메인으로 롤백

--- src/pages/manage/attendance/index.tsx
+++ src/pages/manage/attendance/index.tsx
@@ -1,19 +1,18 @@
-import RoomReservationContainer from "../../../features/manage/RoomReservation/RoomReservationContainer";
-import SightLayout from "../../../layouts/SightLayout";
-import Location from "../../../components/Location";
+import MainLayout from "../../../layouts/MainLayout";
+import AttendanceManageContainer from "../../../features/manage/AttendanceManageContainer";
+import Callout from "../../../components/Callout";
+import { useIsManager } from "../../../hooks/user/useIsManager";
 
-import styles from "./style.module.css";
+export default function ManageAttendancePage() {
+  const { isManager } = useIsManager();
 
-export default function AttendancePage() {
   return (
-    <SightLayout>
-      <Location label="출석 체크 일정 관리" />
-      <main className={styles.content}>
-        <RoomReservationContainer
-          pageTitle="출석 체크 일정 관리"
-          pageSubtitle="운영진용 동아리실 일정 등록 및 주간 일정 확인 페이지입니다."
-        />
-      </main>
-    </SightLayout>
+    <MainLayout>
+      {isManager ? (
+        <AttendanceManageContainer />
+      ) : (
+        <Callout type="error">권한이 부족합니다.</Callout>
+      )}
+    </MainLayout>
   );
 }
--- src/pages/member/attendance.tsx
+++
@@ -1,8 +0,0 @@
-// src/pages/member/attendance.tsx
-export default function AttendancePage() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h2>일반 회원 출석 페이지 (준비 중)</h2>
-    </div>
-  );
-}
